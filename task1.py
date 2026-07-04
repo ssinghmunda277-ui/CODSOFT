28d041636cf8ff5ed0c5b56f63e3834e99f2ffe2
ssinghmunda277-ui <ssinghmunda277@gmail.com>
2026-07-04 16:26:39 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,46 @@
+
+
+tasks = []
+
+while True:
+    print("\n----- TO-DO LIST -----")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == "1":
+        task = input("Enter a new task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+    elif choice == "3":
+        if len(tasks) == 0:
+            print("No tasks to remove.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+            try:
+                task_num = int(input("Enter task number to remove: "))
+                removed = tasks.pop(task_num - 1)
+                print(f"'{removed}' removed successfully!")
+            except:
+                print("Invalid task number.")
+
+    elif choice == "4":
+        print("Exiting To-Do List Application...")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
